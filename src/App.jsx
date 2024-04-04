4e89111b083a1dd1d49434bf7c77b59251791ee7
SanHOWang <wang3harry@gmail.com>
2024-04-04 13:36:30 +0800

    第一次嘗試推程式碼，靜態部分差不多完成，但動畫人物轉換不太知道怎麼寫。

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,94 @@
 import { React, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import main_picture from './pictures/團體照.png';
+import gray_vector from './pictures/Vector.png';
+import person4 from './pictures/personal_picture/01-黃政文.png';
+import person5 from './pictures/personal_picture/02-蘇奕幃.png';
+import person3 from './pictures/personal_picture/03-王三禾.png';
+import person2 from './pictures/personal_picture/04-紹紹.png';
+import person1 from './pictures/personal_picture/05-曾鈺蘋.png';
+import vector from './pictures/personal_picture/Vector.png';
+import button1 from './pictures/personal_picture/button.png';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [displayText, setDisplayText] = useState("");
+  const handleClick = (personId) => {
+    // 根据点击的 personId 区分不同的图片，并显示不同的文本内容
+    switch(personId) {
+      case 'person1':
+        setDisplayText( <>
+          <span style={{ fontSize: '30px' }}>曾鈺蘋</span><br />
+          工程師<br />
+          e-mail: E24103175@gs.ncku.edu.tw
+        </>);
+        break;
+      case 'person2':
+        setDisplayText( <>
+          <span style={{ fontSize: '30px' }}>阮紹銘</span><br />
+          工程師<br />
+          e-mail: bend06b@gmail.com
+        </>);
+        break;
+      case 'person3':
+        setDisplayText( <>
+          <span style={{ fontSize: '30px' }}>王三禾</span><br />
+          工程師<br />
+          e-mail: wang3harry@gmail.com
+        </>);
+        break;
+      case 'person4':
+        setDisplayText( <>
+          <span style={{ fontSize: '30px' }}>黃政文</span><br />
+          UI/UX設計師<br />
+          e-mail: nodong3334@gmail.com
+        </>);
+        break;
+      case 'person5':
+        setDisplayText( <>
+          <span style={{ fontSize: '30px' }}>蘇奕幃</span><br />
+          資深工程師<br />
+          e-mail: yiwei.suuu@gmail.com
+        </>);
+        break;
+      default:
+        setDisplayText(""); 
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <body>
+      <div className="page-container">
+        <div className="people-images">
+          <img src={main_picture} alt="main_picture" />
+        </div>
+        <div className="gray_vector">
+          <img src={gray_vector} alt="gray_vector" />
+        </div>
+        <h className="about_us">
+          Something About Us
+        </h>
+        <p class="introduce">Our UI team aims to create a sleek web interface for credit card profit analysis. With a focus on design and user experience, we're dedicated to delivering intuitive solutions.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="personal_introduce" >
+        <img class="personsmall" id="person1" onClick={() => handleClick('person1')} src={person1} alt="person1" />
+        <img class="personsmall" id="person2" onClick={() => handleClick('person2')} src={person2} alt="person2" />
+        <img class="personsmall" id="person3" onClick={() => handleClick('person3')} src={person3} alt="person3" />
+        <img class="personsmall" id="person4" onClick={() => handleClick('person4')} src={person4} alt="person4" />
+        <img class="personsmall" id="person5" onClick={() => handleClick('person5')} src={person5} alt="person5" />
+        <img id="button1" src={button1} alt="button1" />
+        <div className="right-side">
+          <p>{displayText}</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="vector">
+        <img class="vectorsmall" src={vector} alt="vector1" />
+        <img class="vectorsmall" src={vector} alt="vector2" />
+        <img class="vectorsmall" src={vector} alt="vector3" />
+        <img class="vectorsmall" src={vector} alt="vector4" />
+        <img class="vectorsmall" src={vector} alt="vector5" />
+      </div>
+      
+    </body>
   );
 }
 
